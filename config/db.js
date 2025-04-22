24d26ca9d722ce2e229d6dfdf2da26b7b787061a
kdkritya0330 <72187383+kdkritya0330@users.noreply.github.com>
2025-04-23 02:48:06 +0530

    chang edocker

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect('mongodb+srv://ritya:0330@cluster0.ad1ju.mongodb.net/doctor?retryWrites=true&w=majority');
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
     console.log(`Mongodb Server Issue ${error}`.bgRed.white);
