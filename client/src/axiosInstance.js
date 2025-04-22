1971ce05511edba77dc5cb50210f66ab7ec70494
kdkritya0330 <72187383+kdkritya0330@users.noreply.github.com>
2025-04-22 23:39:00 +0530

    chang edocker

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,4 +1,4 @@
-import axios from "../axiosInstance";
+import axios from "../src/axiosInstance";
 
 const instance = axios.create({
   baseURL: "http://13.127.152.132:8082", // 🔁 Replace with your actual EC2 public IP
