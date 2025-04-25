0fdb4695830ad604e3dff869ade1f94c7998ed92
kdkritya0330 <72187383+kdkritya0330@users.noreply.github.com>
2025-04-25 11:17:36 +0530

    chang edocker

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://15.207.108.158:8082/", // 'backend' is the Docker service name
+  baseURL: "http://43.204.29.197:8082/", // 'backend' is the Docker service name
 });
 
 export default instance;
